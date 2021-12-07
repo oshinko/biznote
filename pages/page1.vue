8d0475489a7c8d2bc0cc0ec2f7419ca79282b8a3
Oshinko <osnk@renjaku.jp>
2021-12-07 20:29:37 +0900

    Update docs

--- pages/page1.vue
+++ pages/page1.vue
@@ -1,12 +1,54 @@
 <template>
-  <div>
-    {{ $route.path }}
-  </div>
+  <div v-html="content"></div>
 </template>
 
 <script>
+import content from '@/assets/page1/content.md'
+import image from '@/assets/page1/1200x630.png'
+import meta from '@/assets/page1/meta.yml'
+
 export default {
-  mounted() {
+  async asyncData({ $config, $marked, route }) {
+    const html = $marked.parse(content)
+
+    return {
+      title: meta.title ?? (html.match(/<h1[^>]+>([^<]+)<\/h1>/) ?? [])[1],
+      content: html,
+      og: {
+        url: `${$config.baseURL}${route.path}`,
+        image: `${$config.baseURL}${image}`
+      }
+    }
+  },
+
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: meta.description
+        },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary_large_image'
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: meta.description
+        },
+        { hid: 'og:image', property: 'og:image', content: this.og.image },
+        { hid: 'og:image:alt', property: 'og:image:alt', content: this.title },
+        { hid: 'og:title', property: 'og:title', content: this.title },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: this.og.url }
+      ]
+    }
   }
 }
 </script>
